a75553506a5aa54d102168211c2c2d804e0ef4aa
aigerim122021-hash <aigerim122021@gmail.com>
2025-12-03 15:05:22 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,15 @@
-import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import os
 
-from os import getenv
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-TOKEN = getenv("TELEGRAM_BOT_TOKEN")
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 
-@dp.message(F.text == "/start")
-async def start(message: Message):
-    await message.answer("Привет! Бот работает.")
-
-async def main():
-    await dp.start_polling(bot)
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Привет! Бот работает.")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp, skip_updates=True)
